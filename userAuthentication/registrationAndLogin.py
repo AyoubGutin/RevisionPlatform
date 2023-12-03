115f8ca51cad01edc8e8087f91470f4f2bdfb59d
Ayoub <gutinayoub@gmail.com>
2023-12-03 22:46:45 +0000

    - Added database (sqllite3). - Fixed path of database. - Replaced dummy variables. with database validation checks. - Only students can register. - Added unittests.

--- userAuthentication/registrationAndLogin.py
+++ userAuthentication/registrationAndLogin.py
@@ -1,17 +1,28 @@
 import tkinter as tk
 from tkinter import messagebox
-import sqlite3   # RDMS
+import sqlite3   # The relational database system I will be using, it is not server based, I can avoid paying costs
+# this way. I will be using a graphical interface to view my tables.
+import os
 
 
 class UserAuthenticationManager:
     def __init__(self):
-        self.conn = sqlite3.connect("user_database.db")
-        self.createTables()
+        # Specifies location of the place I want the database file to  be
+        # This was as it was causing me issues as it was making the database file inside the authentication directory
+        projectRoot = "C:\\Users\\washb\\PycharmProjects\\RevisionPlatform"
+        dbPath = os.path.join(projectRoot, "user_database.db")
+
+        try:
+            self.conn = sqlite3.connect(dbPath)
+            self.createTables()
+        except sqlite3.Error as e:
+            print("SQLITE ERROR", e)
+
 
     def createTables(self):
         cursor = self.conn.cursor()
 
-        # Create a table for students
+        # Creates table for students - this is only needed once if a computer does not have a database file.
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS students (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,12 +32,12 @@ class UserAuthenticationManager:
             )
         """)
 
-        # Create a table for teachers
+        # Create table for teachers - this is only needed once if a computer does not have a database file.
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS teachers (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT UNIQUE,
-                password TEXT
+                password TEXT,
                 email TEXT UNIQUE
             )
         """)
@@ -39,14 +50,14 @@ class UserAuthenticationManager:
         """
         cursor = self.conn.cursor()
 
-        # Check for student login
+        # Attempts to fetch the username and password entered from the database and checks if it returns True.
         cursor.execute(f"SELECT * FROM students WHERE username=? AND password=?", (username, password))
         student = cursor.fetchone()
 
         if student:
             return "student", True
 
-        # Check for teacher login
+        # Attempts to fetch the username and password entered from the database and checks if it returns True.
         cursor.execute("SELECT * FROM teachers WHERE username=? AND password=?", (username, password))
         teacher = cursor.fetchone()
 
@@ -55,39 +66,54 @@ class UserAuthenticationManager:
 
         return None, False
 
-    def registerUser(self, username, password, email, userType):
+    def registerCheck(self, username, password, email, userType):
         """
-        class method for validating a new account
+        class method for validating a new account, responsible for checking database
         """
 
         cursor = self.conn.cursor()
 
         # Check if email is already associated with an account, checks if username is already associated.
         cursor.execute("SELECT * FROM students WHERE email=?", (email,))
-        existingUser = cursor.fetchone()
+        existingStudent = cursor.fetchone()
+
+        print("Existing Student:", existingStudent) # debug: check value
 
         # Basic email validation
-        if existingUser or not email.endswith("@gmail.com"):
-            return False   # email exists or invalid email format
+        if existingStudent or not email.endswith("@gmail.com"):
+            print("Email exists or Email is in wrong format")
+            return False   # Email exists or invalid email format
 
-        # Basic username length check
         if len(password) < 8:
-            return False   # Invalid password
+            print("Invalid password length") # debug check
+            return False   # Invalid password length
 
         if len(username) > 15:
+            print("Invalid username")
             return False   # Invalid username length
 
-        cursor.execute("INSERT INTO students (username, password, email) VALUES (?, ?, ?", (username, password, email))
+        # If the userType is a student, it will be allowed to complete the register function and insert the values into
+        # the database.
+        if userType == "student":
+            cursor.execute("INSERT INTO students (username, password, email) VALUES (?, ?, ?)", (username, password, email))
+        else:
+            return False # cannot register as a teacher or invalid user type.
+
         self.conn.commit()
+        print("Registration successful")
         return True
 
-    def __del__(self):
-        self.conn.close()
+    def closeConnection(self):
+        if hasattr(self, "conn"):
+            self.conn.close()
+
+# Create instance of class so methods can be accessed
+userAuth = UserAuthenticationManager()
 
 def openRegistrationWindow():
     """
     Opens registration window for a better UI
-    Retrieves username and password from user
+    Displays input forms for username, password, email
     Validates this by calling on registerUser()
     """
     registrationWindow = tk.Toplevel(selectionWindow)
@@ -111,28 +137,29 @@ def openRegistrationWindow():
 
     def registerUser():
         """
-        1) retrieves username and password from user
-        2) Validates this
+        1) retrieves username and password from user input
+        2) Validates this by calling on the class method registerCheck() which deals with the database checks.
         """
         enteredUsername = usernameEntry.get()
         enteredPassword = passwordEntry.get()
         confirmPassword = confirmPasswordEntry.get()
         enteredEmail = emailEntry.get()
 
+        # Checks if enteredPassword and confirmPassword are the same.
         if enteredPassword != confirmPassword:
-            messagebox.showerror("Registration Error", "Passwords do not match") # Comparison to check passwords
-        else:
-            userType, success = UserAuthenticationManager.registerUser(enteredUsername, enteredPassword, enteredEmail)
-            if success:
-                messagebox.showinfo("Registration", f"Registration Successful for {userType.capitalize()}")
+            messagebox.showerror("Registration Error", "Passwords do not match")
 
-                if userType == "teacher":
-                    registrationWindow.destroy() # Close teacher registration as not allowed
+        else:
+            # Calls on registerCheck and success = True if database checks return True
+            success = userAuth.registerCheck(enteredUsername, enteredPassword, enteredEmail, "student")
+            if success:
+                messagebox.showinfo("Registration", f"Registration Successful for {enteredUsername.upper()}")
 
             else:
                 messagebox.showerror("Registration Error", "Registration Failed, Try Again. ")
 
             registrationWindow.destroy()
+            userAuth.closeConnection()
 
     registerButton = tk.Button(registrationWindow, text="Register", command=registerUser)
     registerButton.pack(pady=10)
@@ -145,6 +172,7 @@ def displayLogin():
     2) Validates Credentials by calling on the class method authenticateUser
     """
 
+    # Sets up the graphical interface for the login section when a user clicks on login in the homepage.
     loginWindow = tk.Toplevel(selectionWindow)
     loginWindow.title("Login")
 
@@ -161,10 +189,7 @@ def displayLogin():
         enteredUsername = usernameEntry.get()
         enteredPassword = passwordEntry.get()
 
-        # Create instance of class
-        authManager = UserAuthenticationManager()
-
-        userType, success = authManager.authenticateUser(enteredUsername, enteredPassword)
+        userType, success = userAuth.authenticateUser(enteredUsername, enteredPassword)
 
         if success:
             messagebox.showinfo("Authentication", "Login Successful")
---
+++ userAuthentication/testUserAuthentication.py
@@ -0,0 +1,37 @@
+import unittest
+
+import self
+
+from userAuthentication.registrationAndLogin import UserAuthenticationManager, openRegistrationWindow, displayLogin
+
+
+class TestUserAuthentication(unittest.TestCase):
+    def setUp(self):
+        self.authManager = UserAuthenticationManager()
+
+    def tearDown(self):
+        del self.authManager
+
+    def testRegisterUser(self):
+        # Test successful registration
+        result = self.authManager.registerCheck("testUser", "testPassword", "testUser@gmail.com", "student")
+        self.assertTrue(result)
+
+        # Test registration with duplicate email
+        resultExistingEmail = self.authManager.registerCheck("anotherUser", "testPassword", "testUser@gmail.com", "student")
+        self.assertFalse(resultExistingEmail)
+
+    def testLogin(self):
+        # Test successful login
+        self.authManager.registerCheck("testUser2", "testPassword", "testUser2@gmail.com", "student")
+        userType, success = self.authManager.authenticateUser("testUser2", "testPassword")
+        self.assertTrue(success)
+        self.assertEqual(userType, "student")
+
+        # Test unsuccessful login
+        userTypeFail, fail = self.authManager.authenticateUser("nonExistentUser", "testPassword")
+        self.assertFalse(fail)
+        self.assertIsNone(userTypeFail)
+
+if __name__ == "__main__":
+    unittest.main()
