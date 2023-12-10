4405b72dd667d28ca79ee4c595aebe49f9d8407b
Ayoub <gutinayoub@gmail.com>
2023-12-10 12:34:31 +0000

    Added functionality to the buttons and made a teachers hompage Need to give code the opening of the pages depending on user type

--- Homepage/studentHomepage.py
+++ Homepage/studentHomepage.py
@@ -20,14 +20,65 @@ class StudentPage:
         self.heading.pack(fill="x")
 
         # Widgets (Buttons)
-        self.yourSessionsButton = tk.Button(self.frame, text="Your Sessions", height=5, width=15)
+        self.yourSessionsButton = tk.Button(self.frame, text="Your Sessions", height=5, width=15,
+                                            command=self.openYourSessionsWindow)
         self.yourSessionsButton.pack(side="left", padx=15, pady=15)
 
-        self.createNewSessionButton = tk.Button(self.frame, text="Create New Session", height=5, width=15)
+        self.createNewSessionButton = tk.Button(self.frame, text="Create New Session", height=5, width=15,
+                                                command=self.openCreateNewSessionWindow)
         self.createNewSessionButton.pack(side="left", padx=15, pady=15)
 
-        self.newAssignmentsButton = tk.Button(self.frame, text="New Assignments", height=5, width=15)
+        self.newAssignmentsButton = tk.Button(self.frame, text="New Assignments", height=5, width=15,
+                                              command=self.openNewAssignmentsWindow)
         self.newAssignmentsButton.pack(side="right", padx=15, pady=15)
+
+    def openYourSessionsWindow(self):
+        yourSessionsWindow = tk.Toplevel(self.parent)
+        YourSessionsWindow(yourSessionsWindow)
+
+    def openCreateNewSessionWindow(self):
+        createNewSessionWindow = tk.Toplevel(self.parent)
+        CreateNewSessionWindow(createNewSessionWindow)
+
+    def openNewAssignmentsWindow(self):
+        newAssignmentsWindow = tk.Toplevel(self.parent)
+        NewAssignmentsWindow(newAssignmentsWindow)
+
+
+# Class to represent "Your Sessions" window
+class YourSessionsWindow:
+    def __init__(self, parent):
+        self.parent = parent
+        parent.title("Your Sessions")
+        parent.configure(bg=bgColour)
+        parent.geometry("600x400")
+
+        self.frame = tk.Frame(parent, bg=bgColour)
+        self.frame.pack(expand=True)
+
+
+# Class to represent "New Session" window
+class CreateNewSessionWindow:
+    def __init__(self, parent):
+        self.parent = parent
+        parent.title("Create New Session")
+        parent.configure(bg=bgColour)
+        parent.geometry("600x400")
+
+        self.frame = tk.Frame(parent, bg=bgColour)
+        self.frame.pack(expand=True)
+
+
+# Class to represent "New Assignments" window.
+class NewAssignmentsWindow:
+    def __init__(self, parent):
+        self.parent = parent
+        parent.title("New Assignments")
+        parent.configure(bg=bgColour)
+        parent.geometry("600x400")
+
+        self.frame = tk.Frame(parent, bg=bgColour)
+        self.frame.pack(expand=True)
 
 
 def openHomePage():
---
+++ Homepage/teacherHomepage.py
@@ -0,0 +1,91 @@
+import tkinter as tk
+
+bgColour = "#A9C6B8"
+headingColour = "#D9D9D9"
+
+
+# Initialise class to encapsulate the logic specific to a teacher's homepage.
+class TeacherPage:
+    def __init__(self, parent):
+        self.parent = parent
+        parent.title("Homepage")
+        parent.configure(bg=bgColour)
+        parent.geometry("600x400")
+
+        self.frame = tk.Frame(parent, bg=bgColour)
+        self.frame.pack(expand=True)
+
+        # Heading for homepage
+        self.heading = tk.Label(self.frame, text="Homepage", font=("Cairo", 16), bg=headingColour)
+        self.heading.pack(fill="x")
+
+        # Widgets (Buttons)
+        self.yourStudentsButton = tk.Button(self.frame, text="Your Students", height=5, width=15,
+                                            command=self.openYourStudentsButton)
+        self.yourStudentsButton.pack(side="left", padx=15, pady=15)
+
+        self.createNewAssignmentButton = tk.Button(self.frame, text="Create New\nAssignment", height=5, width=15,
+                                                   command=self.openCreateNewAssignmentWindow)
+        self.createNewAssignmentButton.pack(side="left", padx=15, pady=15)
+
+        self.yourAssignmentsButton = tk.Button(self.frame, text="Your Assignments", height=5, width=15,
+                                               command=self.openYourAssignmentsWindow)
+        self.yourAssignmentsButton.pack(side="right", padx=15, pady=15)
+
+    def openYourStudentsButton(self):
+        yourStudentsWindow = tk.Toplevel(self.parent)
+        YourStudentsWindow(yourStudentsWindow)
+
+    def openCreateNewAssignmentWindow(self):
+        createNewAssignmentWindow = tk.Toplevel(self.parent)
+        CreateNewAssignmentWindow(createNewAssignmentWindow)
+
+    def openYourAssignmentsWindow(self):
+        yourAssignmentsWindow = tk.Toplevel(self.parent)
+        YourAssignmentsWindow(yourAssignmentsWindow)
+
+
+# Class to represent "Your Students" window
+class YourStudentsWindow:
+    def __init__(self, parent):
+        self.parent = parent
+        parent.title("Your Students")
+        parent.configure(bg=bgColour)
+        parent.geometry("600x400")
+
+        self.frame = tk.Frame(parent, bg=bgColour)
+        self.frame.pack(expand=True)
+
+
+# Class to represent "Create New Assignment" window
+class CreateNewAssignmentWindow:
+    def __init__(self, parent):
+        self.parent = parent
+        parent.title("Create New Assignment")
+
+        parent.configure(bg=bgColour)
+        parent.geometry("600x400")
+
+        self.frame = tk.Frame(parent, bg=bgColour)
+        self.frame.pack(expand=True)
+
+
+# Class to represent "Your Assignments" window.
+class YourAssignmentsWindow:
+    def __init__(self, parent):
+        self.parent = parent
+        parent.title("Your Assignments")
+        parent.configure(bg=bgColour)
+        parent.geometry("600x400")
+
+        self.frame = tk.Frame(parent, bg=bgColour)
+        self.frame.pack(expand=True)
+
+
+def openHomePage():
+    root = tk.Tk()
+    teacherPage = TeacherPage(root)
+    root.mainloop()
+
+
+openHomePage()
