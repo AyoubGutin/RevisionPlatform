5cce69a566509cce7093b280964615e8eef9329b
Ayoub <gutinayoub@gmail.com>
2023-12-26 17:06:47 +0000

    Added method to compare user answer with correct answer.

--- Questions/activeSession.py
+++ Questions/activeSession.py
@@ -17,11 +17,21 @@ class ActiveSession:
         self.studentID = studentID
 
         self.frame = tk.Frame(parent, bg=bgColour)
-        self.frame.pack(expand=True)
+        self.frame.pack(fill="x")
 
+        # UI elements
         self.heading = tk.Label(self.frame, text="Active Session", font=("Cairo", 16), bg=headingColour)
         self.heading.pack(fill="x")
 
+        self.question = tk.Label(self.frame, text="", font=("Cairo", 16), bg=bgColour)
+        self.question.pack(pady=20)
+
+        self.answerEntry = tk.Entry(self.frame, width=100)
+        self.answerEntry.insert(0, "Enter Answer")
+        self.answerEntry.pack(pady=10, padx=10)
+
+        self.confirmAnswer = tk.Button(self.frame, text="Submit Answer", command=self.compareCorrectAnswer)
+        self.confirmAnswer.pack()
         # Database connection for use in methods.
         projectRoot = "C:\\Users\\washb\\PycharmProjects\\RevisionPlatform"
         dbPath = os.path.join(projectRoot, "user_database.db")
@@ -31,12 +41,29 @@ class ActiveSession:
         except sqlite3.Error as e:
             print("SQLITE ERROR", e)
 
-    def getQuestion(self):
-        projectRoot = "C:\\Users\\washb\\PycharmProjects\\RevisionPlatform"
-        dbPath = os.path.join(projectRoot, "user_database.db")
+        self.getQuestion()
 
-        try:
-            conn = sqlite3.connect(dbPath)
-        except sqlite3.Error as e:
-            print("SQLITE ERROR", e)
+    def getQuestion(self):
+        """
+        Method for retrieving random question and a correct answer.
+        """
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT questionText, answer FROM questions WHERE difficultyLevel = ? ORDER BY RANDOM() LIMIT 1", (self.difficultyLevel,))
+        randomQuestion = cursor.fetchone()
+        self.conn.close()
+
+        self.question.config(text=randomQuestion[0])
+        self.correctAnswer = randomQuestion[1]
+
+    def compareCorrectAnswer(self):
+        """
+        Method for comparing correct answer with user answer
+        """
+        # Retrieve user answer
+        userAnswer = self.answerEntry.get()
+
+        if userAnswer == self.correctAnswer:
+            print("Correct")
+        else:
+            print(f"Wrong, the answer is {self.correctAnswer}")
 
